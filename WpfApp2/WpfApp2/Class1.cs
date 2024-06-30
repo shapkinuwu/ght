667f3bcd9d17fb717bf296dbb4edaaa8cf942db3
shapkineee <145455550+shapkinuwu@users.noreply.github.com>
2024-06-30 16:04:00 +0300

    Add files via upload

---
+++ WpfApp2/WpfApp2/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Class1
+    {
+        Random random = new Random();
+        string[] oblect = { "к", "н", "б" };
+        static Class1 instance;
+        string str;
+        public string Str { get; private set; }
+
+        public static Class1 getInstance()
+        {
+            if (instance == null)
+                instance = new Class1();
+            return instance;
+        }
+        public string rnd()
+        {
+            str = oblect[random.Next(oblect.Length)];
+            return str;
+        }
+        int i=0,pc=0;
+        public string Schet(string str1)
+        {
+            if (str == str1)
+                return $"{i}:{pc}";
+            else
+            {
+                if (str == "к")
+                    if (str1 == "б") i++;
+                    else pc++;
+                else if (str == "н")
+                    if (str1 != "б") i++;
+                    else pc++;
+                else
+                    if (str1 != "н") i++;
+                else pc++;
+            }
+            return $"{i}:{pc}";
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Label1.Content = "0:0";
+        }
+        string object1;
+        Class1 Class1;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool err;
+            switch(object1)
+            {
+                case "к":
+                    к.Visibility = Visibility.Visible;
+                    н.Visibility = Visibility.Hidden;
+                    б.Visibility = Visibility.Hidden;
+                    err =false;
+                    break;
+                case "н":
+                    н.Visibility = Visibility.Visible;
+                    к.Visibility = Visibility.Hidden;
+                    б.Visibility = Visibility.Hidden;
+                    err = false;
+                    break;
+                case "б":
+                    б.Visibility = Visibility.Visible;
+                    к.Visibility = Visibility.Hidden;
+                    н.Visibility = Visibility.Hidden;
+                    err = false;
+                    break;
+                    default:
+                    err = true;
+                    break;
+            }
+            if (err)
+                MessageBox.Show("Ошибка");
+            else
+            {
+                Class1= Class1.getInstance();
+                switch (Class1.rnd())
+                {
+                    case "к":
+                        к_Копировать.Visibility = Visibility.Visible;
+                        н_Копировать.Visibility = Visibility.Hidden;
+                        б_Копировать.Visibility = Visibility.Hidden;
+                        err = false;
+                        break;
+                    case "н":
+                        н_Копировать.Visibility = Visibility.Visible;
+                        к_Копировать.Visibility = Visibility.Hidden;
+                        б_Копировать.Visibility = Visibility.Hidden;
+                        err = false;
+                        break;
+                    case "б":
+                        б_Копировать.Visibility = Visibility.Visible;
+                        н_Копировать.Visibility = Visibility.Hidden;
+                        к_Копировать.Visibility = Visibility.Hidden;
+                        err = false;
+                        break;
+                    default:
+                        err = true;
+                        break;
+                }
+               Label1.Content= Class1.Schet(object1);
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            object1=pressed.Content.ToString();
+        }
+    }
+}
